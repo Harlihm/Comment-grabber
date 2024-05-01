320550b948baa8064290b0f978ac967a7948550f
Harlihm <107759287+Harlihm@users.noreply.github.com>
2024-05-01 04:24:37 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [identifier, setIdentifier] = useState('');
+
   useEffect(() => {
     const fetchData = async () => {
       const options = {
@@ -12,7 +14,7 @@ const App = () => {
       };
 
       const params = new URLSearchParams({
-        identifier: 'YOUR_IDENTIFIER_HERE',
+        identifier: identifier,
         type: 'url', // or 'hash' depending on your requirement
         reply_depth: 2,
         include_chronological_parent_casts: false
@@ -30,11 +32,16 @@ const App = () => {
     };
 
     fetchData();
-  }, []);
+  }, [identifier]);
+
+  const handleInputChange = (event) => {
+    setIdentifier(event.target.value);
+  };
 
   return (
     <div>
       <h1>Fetching Data from API</h1>
+      <input type="text" value={identifier} onChange={handleInputChange} placeholder="Enter Identifier" />
     </div>
   );
 };
